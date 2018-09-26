3e85754438e2f9f9339434032f71f0410b196967
Vu Dao <daoanhvu122@gmail.com>
2018-09-27 04:29:46 +0700

    Config webpack to frontend

--- bkda-frontend/src/app/_helpers/fakebackend.ts
+++ bkda-frontend/src/app/_helpers/fakebackend.ts
@@ -1,3 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
-import {} from 
+import { Observable, of, throwError } from 'rxjs';
+import { delay, mergeMap, materialize, dematerialize } from 'rxjs/operators';
+
+@Injectable()
+export class FakeBackendInterceptor implements HttpInterceptor {
+
+	constructor() {}
+
+	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		let users: any[] = JSON.parse( localStorage.getItem('users') ) || [];
+
+		return of(null).pipe(mergeMap( () => {
+			//
+			if( request.url.endsWith('users/authenticate') && request.method === 'POST' ) {
+
+			}
+		} ));
+
+		return next.handle(request);
+	}
+}
+
+export let FakeBackendProvider = {
+	//
+	provide: HTTP_INTERCEPTORS,
+	useClass: FakeBackendInterceptor,
+	multi: true
+};
--- bkda/src/main/java/com/bkda/controller/UserAPIController.java
+++ bkda/src/main/java/com/bkda/controller/UserAPIController.java
@@ -3,8 +3,6 @@ package com.bkda.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.bkda.dto.ContentResponse;
 import com.bkda.dto.UserDTO;
