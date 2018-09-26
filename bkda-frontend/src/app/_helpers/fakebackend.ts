08177e17ccc1ebc37dca1d06f0fbc43f2313fc25
davu <daoanhvu122@gmail.com>
2018-09-26 07:51:51 +0700

    frontend Angular

---
+++ bkda-frontend/src/app/_helpers/fakebackend.ts
@@ -0,0 +1,3 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {} from 
