6601ccdc1c12d6a63567ad1c19353d1cbebe8873
davu <daoanhvu122@gmail.com>
2018-10-09 11:05:12 +0700

    product list

---
+++ bkda-frontend/src/app/modules/products/productlist/productlist.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'product-list',
+    styleUrls: ['./productlist.component.scss'],
+    templateUrl: './productlist.component.html'
+})
+export class ProductListComponent implements OnInit {
+    
+    constructor(private http: ) {
+
+    }
+    
+    ngOnInit() {
+    }
+}
