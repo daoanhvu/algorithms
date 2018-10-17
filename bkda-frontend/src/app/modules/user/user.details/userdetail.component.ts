6876c01ad7cf5118ff98819e7c50d874e084da9c
davu <daoanhvu122@gmail.com>
2018-10-18 05:02:32 +0700

    userdetail

---
+++ bkda-frontend/src/app/modules/user/user.details/userdetail.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({})
+export class UserDetailComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
