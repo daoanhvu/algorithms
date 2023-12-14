477e5ae299d918b404257fa7cad071992361892c
daoanhvu <daoanhvu122@gmail.com>
2023-12-14 20:55:45 +0700

    addNode

--- trees/bsearch.hpp
+++ trees/bsearch.hpp
@@ -9,12 +9,21 @@ namespace algo {
     };
 
     template <typename T>
-    void addNode(BTreeNode<T> *t, T akey) {
+    BTreeNode<T>* addNode(BTreeNode<T> *t, T akey) {
         if(t == nullptr) {
             t = new BTreeNode<T>;
             t->key = akey;
             t->left = nullptr;
             t->right = nullptr;
+            return t;
+        }
+
+        if(akey < t->key) {
+            return addNode(t->left, akey);
+        }
+
+        if(akey > t->key) {
+            return addNode(t->right, akey);
         }
     }
 
